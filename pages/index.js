8f653b3809f9a48c845c3f5779d006ab19a214c8
Hezha Amen <hezhamen@gmail.com>
2021-07-11 03:03:31 +0300

    WSIN started

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
+  const generateSomething = () => {
+    let num;
+    let genre;
+    let movtv;
+
+    const choices = ["Movie", "TV Show"];
+    const netflixgenres = [
+      "Action & Adventure ",
+      "Anime",
+      "Children & Family",
+      "Classic",
+      "Comedies",
+      "Documentaries",
+      "Dramas",
+      "Horror",
+      "Music",
+      "Romantic",
+      "Sci-fi & Fantasy",
+      "Sports",
+      "Thrillers",
+    ];
+
+    num = Math.floor(Math.random() * 10 + 1);
+    movtv = choices[Math.floor(Math.random() * 2)];
+    genre = netflixgenres[Math.floor(Math.random() * netflixgenres.length)];
+
+    document.getElementById("title").innerHTML = "You should watch...";
+    document.getElementById(
+      "paragraph"
+    ).innerHTML = `the ${num}th ${movtv}, in the ${genre} genre`;
+    document.getElementById("generator").innerHTML = `PICK AGAIN!`;
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="background-card">
+      <div className="background-overlay">
+        <nav>
+          <img
+            src="https://fontmeme.com/permalink/210711/499b51033e97681ca78053d54a6870f2.png"
+            id="logo"
+          />
+        </nav>
+        <div className="main">
+          <h1 id="title">No need to decide!</h1>
+          <p id="paragraph">
+            Let WSIN make the decision of the night for you, enjoy your night.
+          </p>
+          <button id="generator" onClick={generateSomething}>
+            PICK A SHOW!
+          </button>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
